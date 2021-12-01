63f3272beaeba26de8bfa7ffdcf48b60e5efc2a6
ddlan <L.danny18725@gmail.com>
2021-12-01 17:48:05 -0500

    part2 and part 1 is working now ffs

--- app/src/main/kotlin/aoc/App.kt
+++ app/src/main/kotlin/aoc/App.kt
@@ -18,7 +18,26 @@ class Day01 {
         }
 
         fun part2(input: List<String>): Int {
-            return input.size
+            val intPut = input.map{ it.toInt() }
+
+            var count = 0
+            var windowOne = intPut.slice(0..2).sum()
+            var windowTwo = intPut.slice(1..3).sum()
+
+            if (windowTwo > windowOne) {
+                count++
+            }
+
+            for (i in 3..input.size-2) {
+                windowOne = windowOne - intPut[i-3] + intPut[i]
+                windowTwo = windowTwo - intPut[i-2] + intPut[i+1]
+                
+                if (windowTwo > windowOne) {
+                    count++
+                }
+            }
+
+            return count
         }
     }
     
