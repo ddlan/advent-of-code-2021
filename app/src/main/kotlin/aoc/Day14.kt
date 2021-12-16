be1d599e258ac3e5a5f6cb3c72b9fa2d5f43b4e4
ddlan <L.danny18725@gmail.com>
2021-12-16 11:07:14 -0500

    day14 blue eyes white dragon

--- app/src/main/kotlin/aoc/Day14.kt
+++ app/src/main/kotlin/aoc/Day14.kt
@@ -2,70 +2,72 @@ package aoc
 
 class Day14 {
     companion object {
-        private fun readInput(input: List<String>, rules: HashMap<Pair<Char, Char>, Char>): String {
+        private fun readInput(input: List<String>,
+                              rules: HashMap<Pair<Char, Char>, Char>,
+                              count: HashMap<Pair<Char, Char>, Long>): Char {
             val inputIt = input.iterator()
-            val initial = inputIt.next()
+            val chemical = inputIt.next()
             inputIt.next() // blank line
 
             while (inputIt.hasNext()) {
                 val (left, right) = inputIt.next().split(" -> ")
                 rules[Pair(left[0], left[1])] = right[0]
             }
-            return initial
-        }
-
-        private fun polymerize(initial: String, rules: HashMap<Pair<Char, Char>, Char>): String {
-            var additions = ""
-            for (i in 1 until initial.length) {
-                additions += rules[Pair(initial[i-1], initial[i])]
-            }
-
-            return initial.dropLast(1).zip(additions) { a, b ->
-                listOf(a, b)
-            }.flatten().joinToString("") + initial[initial.lastIndex]
-        }
-
-        private fun frequencyDifference(chemical: String): Int {
-            val count = HashMap<Char, Int>()
-
-            for (element in chemical) {
-                count[element] = count.getOrDefault(element, 0) + 1
-            }
-
-            return (count.maxByOrNull { it.value }?.value ?: 0) - (count.minByOrNull { it.value }?.value ?: 0)
-        }
-
-        fun part1(input: List<String>): Int {
-            val rules = HashMap<Pair<Char, Char>, Char>()
-            var curr = readInput(input, rules)
-
-            for (i in 0 until 10) {
-                curr = polymerize(curr, rules)
-            }
-
-            return frequencyDifference(curr)
-        }
-
-//        private fun polymerizeCount(initial: HashMap<Pair<Char, Char>, Int>,
-//                                    rules: HashMap<Pair<Char, Char>, Char>): HashMap<Pair<Char, Char>, Int> {
-//            return null
-//        }
-
-        fun part2(input: List<String>): Int {
-            val rules = HashMap<Pair<Char, Char>, Char>()
-            val chemical = readInput(input, rules)
-            var curr = HashMap<Pair<Char, Char>, Int>()
 
             for (i in 1 until chemical.length) {
                 val pair = Pair(chemical[i-1], chemical[i])
-                curr[pair] = curr.getOrDefault(pair, 0) + 1
+                count[pair] = count.getOrDefault(pair, 0) + 1
+            }
+            return chemical[0]
+        }
+
+        private fun polymerize(initial: HashMap<Pair<Char, Char>, Long>,
+                               rules: HashMap<Pair<Char, Char>, Char>): HashMap<Pair<Char, Char>, Long> {
+            val next = HashMap<Pair<Char, Char>, Long>()
+
+            for ((pair, freq) in initial) {
+                val newElement = rules[pair]!!
+                val left = Pair(pair.first, newElement)
+                val right = Pair(newElement, pair.second)
+                next[left] = next.getOrDefault(left, 0) + freq
+                next[right] = next.getOrDefault(right, 0) + freq
             }
 
-//            for (i in 0 until 40) {
-//                curr = polymerizeCount(curr, rules)
-//            }
+            return next
+        }
 
-            return 0//frequencyDifference(curr)
+        private fun frequencyDifference(count: HashMap<Pair<Char, Char>, Long>, firstElement: Char): Long {
+            val freqs = HashMap<Char, Long>()
+            for ((pair, freq) in count) {
+                freqs[pair.second] = freqs.getOrDefault(pair.second, 0) + freq
+            }
+            freqs[firstElement] = freqs.getOrDefault(firstElement, 0) + 1
+
+            return (freqs.maxByOrNull { it.value }?.value ?: 0) - (freqs.minByOrNull { it.value }?.value ?: 0)
+        }
+
+        fun part1(input: List<String>): Long {
+            val rules = HashMap<Pair<Char, Char>, Char>()
+            var count = HashMap<Pair<Char, Char>, Long>()
+            val first = readInput(input, rules, count)
+
+            for (i in 0 until 10) {
+                count = polymerize(count, rules)
+            }
+
+            return frequencyDifference(count, first)
+        }
+
+        fun part2(input: List<String>): Long {
+            val rules = HashMap<Pair<Char, Char>, Char>()
+            var count = HashMap<Pair<Char, Char>, Long>()
+            val first = readInput(input, rules, count)
+
+            for (i in 0 until 40) {
+                count = polymerize(count, rules)
+            }
+
+            return frequencyDifference(count, first)
         }
     }
 }
